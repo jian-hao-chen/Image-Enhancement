1b305fac4e1d51156c63fde4b287b36a2e789fdf
jian-hao-chen <jhchen850910@icloud.com>
2018-04-26 22:17:50 +0800

    20180426 histogram equalization and inverse implemented.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@
 using namespace cimg_library;
 using namespace std;
 
-typedef CImg<unsigned char> Image;
+typedef CImg<int> Image;
 typedef struct Pixel Pixel;
 
 struct Pixel
 {
-    unsigned char R;
-    unsigned char G;
-    unsigned char B;
+    // color[0] = R, color[1] = G, color[2] = B
+    unsigned char color[3];
     unsigned char Y;
 };
 
@@ -32,16 +31,31 @@ void show_result(const char *file_name[], Image *src)
     }
 }
 
+void inverse(Image *src)
+{
+    for (int h = 0; h < src->height(); h++)
+    {
+        for (int w = 0; w < src->width(); w++)
+        {
+            for (int channel = 0; channel < 3; channel++)
+            {
+                (*src)(w, h, 0, channel) = 255 - (*src)(w, h, 0, channel);
+            }
+        }
+    }
+}
+
 void bubble_sort(unsigned char *src, int length)
 {
     // sort function for median_filter, compare Y value then swap the whole Pixel variable.
+    unsigned char temp;
     for (int i = 0; i < length; i++)
     {
         for (int j = 0; j < length - i; j++)
         {
             if (src[j + 1] < src[j])
             {
-                unsigned char temp = src[j + 1];
+                temp = src[j + 1];
                 src[j + 1] = src[j];
                 src[j] = temp;
             }
@@ -90,32 +104,79 @@ void median_filter(Image *src)
             }
         }
     }
+}
 
-    // // rewrite to the source image
-    // for (int h = 1; h < padding_src.height() - 1; h++)
-    // {
-    //     for (int w = 1; w < padding_src.width() - 1; w++)
-    //     {
-    //         for (int channel = 0; channel < 3; channel++)
-    //         {
-    //             (*src)(w - 1, h - 1, 0, channel) = padding_src(w, h, 0, channel);
-    //         }
-    //     }
-    // }
+void histogram_equalization(Image *src)
+{
+    Image temp, histogram, result;
+    int cdf[256] = {0}, equalized_table[256] = {0};
+    int cdf_min, idx_min;
+    int width = src->width();
+    int height = src->height();
+    int num_pixel = width * height;
+
+    // copy to temp variable and get histogram.
+    temp = src->get_RGBtoYCbCr();
+    histogram = temp.get_channel(0).get_histogram(256, 0, 255);
+
+    // compute cumulative distribution function
+    cdf[0] = histogram(0, 0, 0, 0);
+    for (int scale = 1; scale < 256; scale++)
+    {
+        cdf[scale] = cdf[scale - 1] + histogram(scale, 0, 0, 0);
+    }
+
+    // find cdf minimun value
+    for (int scale = 0; scale < 256; scale++)
+    {
+        if (cdf[scale] > 0)
+        {
+            cdf_min = cdf[scale];
+            idx_min = scale;
+            break;
+        }
+    }
+
+    // equalization and mapping
+    for (int scale = idx_min; scale < 256; scale++)
+    {
+        equalized_table[scale] = (int)((cdf[scale] - cdf_min) * 255.0 / (num_pixel - cdf_min));
+    }
+
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            temp(w, h, 0, 0) = equalized_table[(int)temp(w, h, 0, 0)];
+        }
+    }
+
+    // convert to RGB and rewrite to source.
+    result = temp.get_YCbCrtoRGB();
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < width; w++)
+        {
+            for (int channel = 0; channel < 3; channel++)
+            {
+                (*src)(w, h, 0, channel) = result(w, h, 0, channel);
+            }
+        }
+    }
 }
 
 int main()
 {
     const char *file_name[NUM_TOTAL_IMAGE] = {"Data/1.png", "Data/2.png", "Data/3.png"};
     Image src[NUM_TOTAL_IMAGE];
-
     read_data(file_name, src);
 
-    // do something else ...
-    for (int idx = 0; idx < NUM_TOTAL_IMAGE; idx++)
-    {
-        median_filter(&(src[idx]));
-    }
+    // first image
+    // second image
+    median_filter(&src[1]);
+
+    // third image
+    inverse(&src[2]);
 
     show_result(file_name, src);
 
